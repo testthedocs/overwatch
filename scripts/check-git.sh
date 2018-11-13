a8a7c06a66ff126ddd6cde85293a34aa10daac5f
sven <sven@so36.net>
2018-11-13 14:46:32 +0100

    [scripts] Improve qa

--- scripts/check-git.sh
+++ scripts/check-git.sh
@@ -22,9 +22,9 @@
 # Vars
 ESC_SEQ="\x1b["
 COL_RESET=$ESC_SEQ"39;49;00m"
-COL_YELLOW=$ESC_SEQ"33;01m"
+#COL_YELLOW=$ESC_SEQ"33;01m"
 COL_RED=$ESC_SEQ"31;01m"
-COL_GREEN=$ESC_SEQ"32;01m"
+#COL_GREEN=$ESC_SEQ"32;01m"
 
 gitRepo="."
 # Check the passed params to avoid disapointment
