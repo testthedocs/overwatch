77bf50e4a6be58d29140c7a5b09547cdd1fb2752
sven <sven@so36.net>
2019-03-07 08:39:41 +0100

    [code] Add another changelog check script

---
+++ scripts/ci/changelog.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# If any command inside script returns error, exit and return that error
+set -e
+
+# This file does:
+# Checking for a file called CHANGES.rst
+# Checks is that file got updated with the latest commit
+
+CHECK_FILE=CHANGES.rst
+
+# If there is no file called CHANGES.md let the test fail.
+if [ ! -f "$CHECK_FILE" ]; then
+    echo "Can't find CHANGES.rst, please check"
+    exit 1
+else
+    if git diff HEAD~ --name-only | grep --quiet $CHECK_FILE; then
+        : # Do nothing
+    else
+        echo "Please update the changelog file"
+        echo "Failed!" && exit 1
+    fi
+fi
+exit 0
